455cf6fc8e7018a3794e72e4dcbeb85eefe236cf
Mahi Garg <mahi7garg@gmail.com>
2023-09-11 09:31:13 +0530

    Initial Commit

---
+++ FindingFalcone/FindingFalconeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindingFalconeApp.swift
+//  FindingFalcone
+//
+//  Created by Mahi Garg on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FindingFalconeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
